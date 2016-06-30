ddbe72b06f6dd79f41a3e48267902d3facb2d62e
Geofrey Nyabuto <mwambage@gmail.com>
2016-06-30 17:22:15 +0300

    Final code
    here is the final source code

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -8,8 +8,32 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+typedef enum{ Plus,Minus,Multiply,Divide} CalcOperation;
 
+
+@interface ViewController : UIViewController{
+NSString *storage;
+CalcOperation operation;
+    IBOutlet UITextField *answer;
+}
+
+- (IBAction)no7:(UIButton *)sender;
+- (IBAction)no8:(UIButton *)sender;
+- (IBAction)no9:(UIButton *)sender;
+- (IBAction)divide:(UIButton *)sender;
+- (IBAction)no4:(UIButton *)sender;
+- (IBAction)no5:(UIButton *)sender;
+- (IBAction)no6:(UIButton *)sender;
+- (IBAction)multiply:(UIButton *)sender;
+- (IBAction)no1:(UIButton *)sender;
+- (IBAction)no2:(UIButton *)sender;
+- (IBAction)no3:(UIButton *)sender;
+- (IBAction)minus:(UIButton *)sender;
+- (IBAction)no0:(UIButton *)sender;
+- (IBAction)dot:(UIButton *)sender;
+- (IBAction)plus:(UIButton *)sender;
+- (IBAction)equalsSign:(UIButton *)sender;
+- (IBAction)btnClear:(UIButton *)sender;
 
 @end
 
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -24,4 +24,108 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)no7:(id)sender {
+    answer.text=[NSString stringWithFormat:@"%@7",answer.text];
+
+}
+
+- (IBAction)no8:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@8",answer.text];
+
+}
+
+- (IBAction)no9:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@9",answer.text];
+
+}
+
+- (IBAction)divide:(UIButton *)sender {
+    operation = Divide;
+    storage = answer.text;
+    answer.text=@"";
+
+}
+
+- (IBAction)no4:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@4",answer.text];
+
+}
+
+- (IBAction)no5:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@5",answer.text];
+
+}
+
+- (IBAction)no6:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@6",answer.text];
+
+}
+
+- (IBAction)multiply:(UIButton *)sender {
+    operation = Multiply;
+    storage = answer.text;
+    answer.text=@"";
+
+}
+
+- (IBAction)no1:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@1",answer.text];
+
+}
+
+- (IBAction)no2:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@2",answer.text];
+
+}
+
+- (IBAction)no3:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@3",answer.text];
+
+}
+
+- (IBAction)minus:(id)sender {
+    operation = Minus;
+    storage = answer.text;
+    answer.text=@"";
+
+}
+
+- (IBAction)no0:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@0",answer.text];
+
+}
+
+- (IBAction)dot:(UIButton *)sender {
+    answer.text=[NSString stringWithFormat:@"%@.",answer.text];
+
+}
+
+- (IBAction)plus:(UIButton *)sender {
+    operation = Plus;
+    storage = answer.text;
+    answer.text=@"";
+
+}
+
+- (IBAction)equalsSign:(UIButton *)sender {
+    NSString *val = answer.text;
+    switch(operation) {
+        case Plus :
+            answer.text= [NSString stringWithFormat:@"%qi",[val longLongValue]+[storage longLongValue]];
+            break;
+        case Minus:
+            answer.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]-[val longLongValue]];
+            break;
+        case Divide:
+            answer.text= [NSString stringWithFormat:@"%qi",[storage longLongValue]/[val longLongValue]];
+            break;
+        case Multiply:
+            answer.text= [NSString stringWithFormat:@"%qi",[val longLongValue]*[storage longLongValue]];
+            break;
+    }
+}
+
+- (IBAction)btnClear:(UIButton *)sender {
+    answer.text = @"";
+}
 @end
